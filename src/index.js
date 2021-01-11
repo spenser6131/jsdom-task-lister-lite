e8b26255eeaaaba95035044ddbd277a6b8bc40fa
spenser6131 <brinkman.spenser@gmail.com>
2021-01-10 20:56:07 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-
   let taskForm = document.querySelector("form#create-task-form");
-  let taskDescription = document.querySelector("input#new-task-description");
-  let taskPriority = document.querySelector("select#new-task-priority");
   let tasksList = document.querySelector("ul#tasks");
-
+  
   taskForm.addEventListener("submit", submitForm);
   tasksList.addEventListener("click", removeListItem);
-
+  
   function submitForm(e) {
     e.preventDefault();
-    let newTask = taskDescription.value;
-    let newPriority = taskPriority.value;
-    tasksList.innerHTML += `<li class="${newPriority}">${newTask}</li>`;
+    let taskDescription = document.querySelector("input#new-task-description").value;
+    let taskPriority = document.querySelector("select#new-task-priority").value;
+    let taskDueDate = document.querySelector("input#new-task-due-date").value;
+    tasksList.innerHTML += `<li class="${taskPriority}">${taskDescription}<br>Due ${taskDueDate}</li>`;
     taskForm.reset();
   };
 
